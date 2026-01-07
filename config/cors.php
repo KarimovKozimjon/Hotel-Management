ce25c9c768a53b0e3ac44c651c23cb2d8748054b
Kozimjon Karimov <kozimjonk224@gmail.com>
2026-01-07 20:36:22 +0500

    error resolved

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,9 @@
 <?php
 
+
 $allowedOrigins = (string) env(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:5173,http://127.0.0.1:5173,http://localhost:3000,http://127.0.0.1:3000'
+    'http://localhost:5173,http://127.0.0.1:5173,http://localhost:3000,http://127.0.0.1:3000,https://comfort-hub.netlify.app'
 );
 
 $allowedOriginsList = array_values(array_filter(array_map(
